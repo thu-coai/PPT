9bd3ffdbc0b450629eabbac7eebfba313b8d35bb
gyxbndsthu <guyx21@mails.tsinghua.edu.cn>
2022-08-05 10:40:04 +0800

    update preprocess files

--- scripts/tools/preprocess_pretrain_nsp.sh
+++ scripts/tools/preprocess_pretrain_nsp.sh
@@ -1,12 +1,13 @@
 WOKRING_DIR=/home/guyuxian/PPT-origin/
+INPUT_PATH=${WOKRING_DIR}/pretrain_data/raw/
 
 
 OPTS=""
-OPTS+=" --input ${WOKRING_DIR}/pretrain_data/raw/openwebtext.txt"
-OPTS+=" --input2 ${WOKRING_DIR}/pretrain_data/raw/openwebtext_2.txt"
+OPTS+=" --input ${INPUT_PATH}/openwebtext.txt"
+OPTS+=" --input2 ${INPUT_PATH}/openwebtext_2.txt"
 OPTS+=" --tokenizer_path ${WORKING_DIR}/vocab_en"
 OPTS+=" --output_path ${WOKRING_DIR}/pretrain_data/preprocessed/"
 
 
 shuf ${INPUT_PATH}/openwebtext.txt -o ${INPUT_PATH}/openwebtext_2.txt
-python3 ${WOKRING_DIR}tools/preprocess_pretrain_data_nsp.py ${OPTS}
+python3 ${WOKRING_DIR}/tools/preprocess_pretrain_data_nsp.py ${OPTS}
--- scripts/tools/preprocess_pretrain_nss_uni.sh
+++ scripts/tools/preprocess_pretrain_nss_uni.sh
@@ -1,12 +1,12 @@
 WOKRING_DIR=/home/guyuxian/PPT-origin/
-
+INPUT_PATH=${WOKRING_DIR}/pretrain_data/raw/
 
 OPTS=""
-OPTS+=" --input ${WOKRING_DIR}/pretrain_data/raw/openwebtext.txt"
-OPTS+=" --input_neg1 ${WOKRING_DIR}/pretrain_data/raw/openwebtext_1.txt"
-OPTS+=" --input_neg2 ${WOKRING_DIR}/pretrain_data/raw/openwebtext_2.txt"
-OPTS+=" --input_neg3 ${WOKRING_DIR}/pretrain_data/raw/openwebtext_3.txt"
-OPTS+=" --input_neg4 ${WOKRING_DIR}/pretrain_data/raw/openwebtext_4.txt"
+OPTS+=" --input ${INPUT_PATH}/openwebtext.txt"
+OPTS+=" --input_neg1 ${INPUT_PATH}/openwebtext_1.txt"
+OPTS+=" --input_neg2 ${INPUT_PATH}/openwebtext_2.txt"
+OPTS+=" --input_neg3 ${INPUT_PATH}/openwebtext_3.txt"
+OPTS+=" --input_neg4 ${INPUT_PATH}/openwebtext_4.txt"
 OPTS+=" --tokenizer_path ${WORKING_DIR}/vocab_en"
 OPTS+=" --output_path ${WOKRING_DIR}/pretrain_data/preprocessed/"
 
@@ -15,4 +15,4 @@ shuf ${INPUT_PATH}/openwebtext.txt -o ${INPUT_PATH}/openwebtext_1.txt
 shuf ${INPUT_PATH}/openwebtext.txt -o ${INPUT_PATH}/openwebtext_2.txt
 shuf ${INPUT_PATH}/openwebtext.txt -o ${INPUT_PATH}/openwebtext_3.txt
 shuf ${INPUT_PATH}/openwebtext.txt -o ${INPUT_PATH}/openwebtext_4.txt
-python3 ${WOKRING_DIR}tools/preprocess_pretrain_data_nss_uni.py ${OPTS}
+python3 ${WOKRING_DIR}/tools/preprocess_pretrain_data_nss_uni.py ${OPTS}
